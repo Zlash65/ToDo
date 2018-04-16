695e275be1f5bc063297a87ba6407ce3ae0de794
Zlash65 <zarrar65@gmail.com>
2018-04-16 15:01:03 +0530

    back button behaviour, frappe logo, popover ui fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,56 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, ToastController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { App } from 'ionic-angular';
 
 import { HomePage } from '../pages/home/home';
+import { ToDoListPage } from '../pages/to-do-list/to-do-list';
+import { DisplayToDoPage } from '../pages/display-to-do/display-to-do';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+	rootPage:any = HomePage;
+	lastBack:any;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
-  }
+	constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private app: App,
+		public toastCtrl: ToastController) {
+
+		platform.ready().then(() => {
+		// Okay, so the platform is ready and our plugins are available.
+		// Here you can do any higher level native things you might need.
+		statusBar.styleDefault();
+		splashScreen.hide();
+	
+		platform.registerBackButtonAction(fn => {
+				const overlay = this.app._appRoot._overlayPortal.getActive();
+				const nav = this.app.getActiveNav();
+
+				if(overlay && overlay.dismiss) {
+					overlay.dismiss();
+				}
+				else if(nav.getActive().instance instanceof HomePage
+					|| nav.getActive().instance instanceof ToDoListPage) {
+						if(Date.now() - this.lastBack < 500) {
+							platform.exitApp();
+						} else {
+							let toast = this.toastCtrl.create({
+								message: 'Press back twice to exit',
+								duration: 1000,
+								position: 'bottom'
+							});
+							
+							toast.present();
+						}
+				}
+				else if(nav.getActive().instance instanceof DisplayToDoPage) {
+					nav.pop();
+				}
+
+				this.lastBack = Date.now();
+			})
+		});
+	}
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,46 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { IonicStorageModule } from '@ionic/storage';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ToDoListPage } from '../pages/to-do-list/to-do-list';
+import { DisplayToDoPage } from '../pages/display-to-do/display-to-do';
+import { PopoverComponent } from '../components/popover/popover';
+import initFrappe from "../frappe";
+import { Network } from '@ionic-native/network';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+	HomePage,
+	ToDoListPage,
+	DisplayToDoPage,
+	PopoverComponent
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+	IonicModule.forRoot(MyApp),
+	IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+	HomePage,
+	ToDoListPage,
+	DisplayToDoPage,
+	PopoverComponent
   ],
   providers: [
     StatusBar,
-    SplashScreen,
+	SplashScreen,
+	Network,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
-export class AppModule {}
+export class AppModule {
+	constructor() {
+		initFrappe();
+	};
+}
